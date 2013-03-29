1efe2ea2ca6c599dd054a355ff1e5e7ddb706071
Larry Graham <doc1623@gmail.com>
2013-03-29 10:32:16 -0500

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,42 +4,45 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	  describe "Home page" do
+	 describe "Home page" do
 
-		      it "should have the content 'Sample App'" do
+		    	it "should have the content 'Sample App'" do
 			            visit '/static_pages/home'
-				          page.should have_content('Sample App')
-					      end
-		        end
+				    page.should have_selector('h1', :text => 'Sample App')
+		      	end
 	    		it "should have the right title" do
 		      		visit '/static_pages/home'
 		        	page.should have_selector('title',
-						                      :text => "Ruby on Rails Tutorial Sample App | Home")
+						                      :text => "Ruby on Rails Tutorial Sample App")
 	    		end
-
-	    describe "Help page" do
+			it "should not have a custom page title" do
+			      visit '/static_pages/home'
+			      page.should_not have_selector('title', :text => '| Home')
+			end
+	end
+	describe "Help page" do
 
 		        it "should have the content 'Help'" do
 				      visit '/static_pages/help'
 				            page.should have_content('Help')
-					        end
-			  end
+		  	end
 	    		it "should have the right title" do
 		      		visit '/static_pages/help'
 		        	page.should have_selector('title',
 						                      :text => "Ruby on Rails Tutorial Sample App | Help")
 	    		end
-	    describe "About page" do
+	end
+	describe "About page" do
 
 		        it "should have the content 'About Us'" do
 				      visit '/static_pages/about'
 				            page.should have_content('About Us')
-					        end
 			  end
 	    		it "should have the right title" do
 		      		visit '/static_pages/about'
 		        	page.should have_selector('title',
 						                      :text => "Ruby on Rails Tutorial Sample App | About")
 	    		end
+	end
 
 end
